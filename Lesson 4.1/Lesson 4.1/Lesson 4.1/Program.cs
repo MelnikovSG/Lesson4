b9d9547ff4530d25bc442a75dbfdc2ae46964d5b
MelnikovSG <57829903+MelnikovSG@users.noreply.github.com>
2021-03-04 22:55:51 +0700

    HW2

--- Lesson 4.1/Lesson 4.1/Lesson 4.1/Program.cs
+++ Lesson 4.1/Lesson 4.1/Lesson 4.1/Program.cs
@@ -9,55 +9,7 @@ namespace Lesson_4._1
         {
 
             Console.WriteLine("Здравствуйте. Вас приветствует математическая программа");
-            Solution.EnterNumber();
-            
+            EnterNumber.Enter();
         }
-    
-    public class Solution
-        {
-            public static void EnterNumber()
-            {
-
-                    Console.WriteLine("пожалуйста введите число: ");
-                    string value = Console.ReadLine();
-                    int number;
-
-                    bool isNum = int.TryParse(value, out number);
-
-                    if (isNum)
-                        {
-                            Sol(number);
-
-                        } else
-                            {
-                                Console.WriteLine("Вы ошиблись!");
-                                EnterNumber();
-                            }
-
-            }
-
-            public static void Sol(int num)
-            {
-
-                int num1 = 1, num2 = 0, num3 = 0;
-                
-                for (int i = 1; i <= num; i++)
-                {
-                    num1 *= i;
-                    num2 += i;
-                    if (i % 2 == 0)
-                    {
-                        num3 = i;
-                    }
-                }
-
-                Console.WriteLine ($"Факториал равен {num1} Сумма от 1 до N равна {num2}");
-
-                Console.WriteLine ($"Максимальное четное число меньше N равно {num3}");
-
-                Console.ReadLine();
-            }
-        }
-        
     }
 }
---
+++ Lesson 4.1/Lesson 4.1/Lesson 4.1/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Lesson_4._1
+{
+    public class Solution
+    {
+        public static void Sol(int num)
+        {
+            int num1 = 1, num2 = 0, num3 = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                num1 *= i;
+                num2 += i;
+                if (i % 2 == 0)
+                {
+                    num3 = i;
+                }
+            }
+
+            Console.WriteLine($"Факториал равен {num1} Сумма от 1 до N равна {num2}");
+
+            Console.WriteLine($"Максимальное четное число меньше N равно {num3}");
+
+            Console.ReadLine();
+        }
+    }
+
+}
